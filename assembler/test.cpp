d77e61ec28f9cb4e0adac515ed4b0f9c3fb936c5
Ruben Saunders <53256494+triflicacid@users.noreply.github.com>
2024-11-09 18:05:56 +0000

    removed assembler-test

--- assembler/test.cpp
+++ assembler/test.cpp
@@ -4,29 +4,29 @@
 #include <util.hpp>
 
 int main() {
-  std::string str = "1, 2";
-  int i = 0;
-  bool is_double;
-  uint64_t value;
+    std::string str = "1, 2";
+    int i = 0;
+    bool is_double;
+    uint64_t value;
 
-  std::cout << "string: '" << str << "'\n";
-  bool ok = parse_number(str, i, value, is_double);
+    std::cout << "string: '" << str << "'\n";
+    bool ok = parse_number(str, i, value, is_double);
 
-  if (ok) {
-    std::cout << "is double = " << is_double << "\n";
-    std::cout << "value = ";
+    if (ok) {
+        std::cout << "is double = " << is_double << "\n";
+        std::cout << "value = ";
 
-    if (is_double) {
-      std::cout << *(double *) &value;
+        if (is_double) {
+            std::cout << *(double *) &value;
+        } else {
+            std::cout << value;
+        }
+
+        std::cout << "\nindex = " << i << "\n";
     } else {
-      std::cout << value;
+        std::cout << "failure\n";
     }
+    std::cout << "remainder: '" << str.substr(i) << "'\n";
 
-    std::cout << "\nindex = " << i << "\n";
-  } else {
-    std::cout << "failure\n";
-  }
-  std::cout << "remainder: '" << str.substr(i) << "'\n";
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
