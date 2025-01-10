56dbffcf1077783d0447a73076c130f410cc90cb
Ruben Saunders <53256494+triflicacid@users.noreply.github.com>
2025-01-10 17:51:23 +0000

    updated .gitignore

--- language-old/src/LanguageOptions.cpp
+++
@@ -1,7 +0,0 @@
-namespace language::options {
-  char default_entry_point[] = "main";
-  bool allow_alter_entry = true;
-  bool allow_shadowing = true;
-  bool must_declare_functions = false;
-  int unused_symbol_level = 1;
-}
